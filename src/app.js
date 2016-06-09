a0b613f31ca8193f9b704ed67ab935a691d00f79
bryantbello <bryant.bello@outlook.com>
2016-06-09 18:28:37 -0400

    webcfg

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './app.css'
+
+import 'font-awesome/css/font-awesome.css'
+
+
 
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>);
+    return (
+      <div className={styles.wrapper}>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}</h1>
+      </div>
+    )
   }
 });
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
