b80af76381bed441d531e4189518c0dee734fd6b
bryantbello <bryant.bello@outlook.com>
2016-06-10 21:57:23 -0400

    containerJS

--- src/app.js
+++ src/app.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './app.css'
-import styles from './styles.module.css'
 
 import 'font-awesome/css/font-awesome.css'
+import './app.css'
 
-//create class component
+import App from 'containers/App/App'
 
-const App = React.createClass({
-  render: function() {
-    return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}</h1>
-      </div>
-    )
-  }
-});
+import {hashHistory} from 'react-router'
+import makeRoutes from './routes'
+
+const routes = makeRoutes()
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <App history={hashHistory}
+        routes={routes} />,
+mountNode);
